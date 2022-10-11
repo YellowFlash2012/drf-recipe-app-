0dbc3d1830239506e9749402eb8cbfa69990bedf
YellowFlash2012 <cyclop9@hotmail.com>
2022-10-11 07:06:15 +0100

    designed and implemented fetching 1 single recipe and create a new recipe

--- recipe/views.py
+++ recipe/views.py
@@ -8,10 +8,19 @@ from recipe import serializers
 
 # Create your views here.
 class AllUserRecipeViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return self.queryset.filter(user = self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        return serializer.save(user = self.request.user)
