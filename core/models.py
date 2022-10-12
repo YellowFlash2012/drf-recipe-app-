5a514692a1eae457ae3879662634417487d69c30
YellowFlash2012 <cyclop9@hotmail.com>
2022-10-12 07:08:53 +0100

    designed and implementeent routes

--- core/models.py
+++ core/models.py
@@ -3,6 +3,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 
 from django.conf import settings
+import uuid
+import os
+
+def recipe_image_file_path(instance,filename):
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uu()}{ext}'
+
+    return os.path.join('uploads', 'recipe', 'filename')
 
 class UserManager(BaseUserManager):
     def create_user(self,email,password=None,**extra_fields):
@@ -49,6 +57,8 @@ class Recipe(models.Model):
 
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+
+    image = models.ImageField(null=True,upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
--- recipeproject/urls.py
+++ recipeproject/urls.py
@@ -5,6 +5,9 @@ from django.urls import path, include
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -20,3 +23,9 @@ urlpatterns = [
     # recipe app urls
     path('api/v1/recipes/', include('recipe.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root = settings.MEDIA_ROOT
+    )
